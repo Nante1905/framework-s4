1fcc798ba619e092c06e9151bcb2d73bce056332
Nante <nantemino15@gmail.com>
2023-03-13 21:29:52 +0300

    [create] adding initialize features for frontservlet

---
+++ src/etu1752/framework/decorators/App.java
@@ -0,0 +1,14 @@
+package etu1752.framework.decorators;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface App {
+    public String url();
+    public String method();
+}
--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,10 +1,28 @@
 package utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 public class Utils {
     
     public static String getUrlPath(HttpServletRequest req) {
         return req.getPathInfo();
+    }
+
+    public static Vector<String> getAllClass(File[] files, Vector<String> result) throws IOException {
+        for (File file : files) {
+            if(file.isDirectory()) {
+                getAllClass(file.listFiles(), result);
+            }
+            else {
+                String name = file.getAbsolutePath().replace("\\", ".");
+                int index = name.indexOf(".classes.");
+                result.add(name.substring(index+9, name.length()-6));
+            }
+        }
+        return result;
     }
 }
