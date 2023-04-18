11137e86578734058c4c709bd7e7a0b39e27fdd6
Nante <nantemino15@gmail.com>
2023-04-18 10:08:44 +0300

    [update] refactoring

--- framework/src/etu1752/framework/servlet/FrontServlet.java
+++ framework/src/etu1752/framework/servlet/FrontServlet.java
@@ -39,27 +39,18 @@ public class FrontServlet extends HttpServlet {
         } catch (ClassNotFoundException | IOException e) {
             
         }
-        // try {
-        // this.initialize(this.mappingUrls);
-        // } catch (ClassNotFoundException | IOException e) {
-        // e.printStackTrace();
-        // }
     }
 
     private void initialize(HashMap<String, Mapping> urlsMap) throws IOException, ClassNotFoundException {
         List<Class<Object>> cls = new ArrayList<Class<Object>>();
         URL url = getServletContext().getResource("/WEB-INF/classes");
         String path = url.toString().replace("%20", " ").substring(6);
-        // out.println(path);
         File dir = new File(path);
-        // out.println("existing : " + dir.exists());
         File[] files = dir.listFiles();
-        // out.print(files.length);
         Vector<String> classNames = new Vector<>();
         Utils.getAllClass(files, classNames);
 
         for (String className : classNames) {
-            // out.println(className);
             cls.add((Class<Object>) Class.forName(className));
         }
 
@@ -73,11 +64,6 @@ public class FrontServlet extends HttpServlet {
                 }
             }
         }
-
-        // for (Map.Entry<String,String> u : urlsMap.entrySet()) {
-        // System.out.println(u.getKey() + " => " + u.getValue());
-        // }
-        // }
     }
 
     private void processRequest(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
@@ -86,9 +72,7 @@ public class FrontServlet extends HttpServlet {
         out.println("path url => " + path);
         out.println("url debug "+req.getRequestURI());
 
-        // out.println(this.mappingUrls.size());
         for (Map.Entry<String, Mapping> u : this.mappingUrls.entrySet()) {
-            // out.println(u.getKey() + " => " + u.getValue().getClassName() + " " + u.getValue().getMethod());
             if(u.getKey().equals(path)) {
                 try {
                     Class cls = (Class) Class.forName(u.getValue().getClassName());
