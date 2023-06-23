97a6fbd8eec89c76cc6ae3bfa83f27cf1150aad1
Nante <nantemino15@gmail.com>
2023-06-23 09:40:42 +0300

    [add] add json features annotations

--- framework/src/etu1752/framework/servlet/FrontServlet.java
+++ framework/src/etu1752/framework/servlet/FrontServlet.java
@@ -18,6 +18,7 @@ import java.util.Vector;
 import com.google.gson.Gson;
 
 import etu1752.framework.Mapping;
+import etu1752.framework.decorators.Api;
 import etu1752.framework.decorators.Auth;
 import etu1752.framework.decorators.Params;
 import etu1752.framework.decorators.Scope;
@@ -39,10 +40,9 @@ import utils.Utils;
  * FrontServlet
  */
 
-
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
-maxFileSize = 1024 * 1024 * 10,      // 10 MB
-maxRequestSize = 1024 * 1024 * 100)  // 100 MB)
+        maxFileSize = 1024 * 1024 * 10, // 10 MB
+        maxRequestSize = 1024 * 1024 * 100) // 100 MB)
 
 @WebServlet(name = "FrontServlet", urlPatterns = { "*.etu", "/" })
 public class FrontServlet extends HttpServlet {
@@ -58,11 +58,10 @@ public class FrontServlet extends HttpServlet {
             this.singletons = new HashMap<>();
             this.initialize(this.mappingUrls);
         } catch (ClassNotFoundException | IOException e) {
-            
+
         }
     }
 
-    
     private void initialize(HashMap<String, Mapping> urlsMap) throws IOException, ClassNotFoundException {
         List<Class<Object>> cls = new ArrayList<Class<Object>>();
         URL url = getServletContext().getResource("/WEB-INF/classes");
@@ -71,13 +70,13 @@ public class FrontServlet extends HttpServlet {
         File[] files = dir.listFiles();
         Vector<String> classNames = new Vector<>();
         Utils.getAllClass(files, classNames);
-        
+
         for (String className : classNames) {
             cls.add((Class<Object>) Class.forName(className));
         }
-        
+
         for (Class<?> c : cls) {
-            if(c.isAnnotationPresent(Scope.class)) {
+            if (c.isAnnotationPresent(Scope.class)) {
                 singletons.put(c.getName(), null);
             }
 
@@ -100,26 +99,26 @@ public class FrontServlet extends HttpServlet {
     private void processRequest(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         PrintWriter out = res.getWriter();
         String path = Utils.getUrlPath(req);
-        
+
         // debug
         // out.println("path url => " + path);
         // out.println("url debug "+req.getRequestURI());
-        
+
         for (Map.Entry<String, Mapping> u : this.mappingUrls.entrySet()) {
             // debug
             // out.println(u.getValue().getMethod());
             // for(Class<?> c : u.getValue().getParamsTypes()) {
-            //     out.println(c.getName());
+            // out.println(c.getName());
             // }
-            if(u.getKey().equals(path)) {
+            if (u.getKey().equals(path)) {
                 try {
                     Class<?> cls = (Class<?>) Class.forName(u.getValue().getClassName());
                     Method method = cls.getDeclaredMethod(u.getValue().getMethod(), u.getValue().getParamsTypes());
 
                     Object o = null;
 
-                    if(this.singletons.containsKey(cls.getName())) {
-                        if(this.singletons.get(cls.getName()) == null) {
+                    if (this.singletons.containsKey(cls.getName())) {
+                        if (this.singletons.get(cls.getName()) == null) {
                             o = cls.getConstructor().newInstance();
                             this.singletons.put(cls.getName(), o);
                         } else {
@@ -127,33 +126,31 @@ public class FrontServlet extends HttpServlet {
                         }
                     }
 
-                    if(o == null) {
+                    if (o == null) {
                         o = cls.getConstructor().newInstance();
                     }
-                    
+
                     // data binding fields
 
                     Field[] fields = o.getClass().getDeclaredFields();
-                    for(Field f : fields) {
+                    for (Field f : fields) {
                         String fName = f.getName();
-                        Method setter = o.getClass().getDeclaredMethod("set"+toCamel(fName), f.getType());
+                        Method setter = o.getClass().getDeclaredMethod("set" + toCamel(fName), f.getType());
 
-
-                        if(req.getContentType() != null && req.getContentType().startsWith("multipart/")) {
-                            if(f.getType() == FileUpload.class) {
-                                for(Part part : req.getParts()) {
-                                    if(fName.equals(part.getName())) {
+                        if (req.getContentType() != null && req.getContentType().startsWith("multipart/")) {
+                            if (f.getType() == FileUpload.class) {
+                                for (Part part : req.getParts()) {
+                                    if (fName.equals(part.getName())) {
                                         FileUpload file = new FileUpload();
                                         file.setName(part.getSubmittedFileName());
                                         file.setBytes(part.getInputStream().readAllBytes());
-    
+
                                         setter.invoke(o, file);
                                     }
                                 }
-                            } 
-                        }
-                        else {
-                            if(req.getParameter(fName) != null && req.getParameter(fName) != "") {
+                            }
+                        } else {
+                            if (req.getParameter(fName) != null && req.getParameter(fName) != "") {
                                 // out.print(req.getParameter(fName));
                                 Object value = Utils.strToObject(req.getParameter(fName), f.getType());
                                 setter.invoke(o, value);
@@ -162,31 +159,31 @@ public class FrontServlet extends HttpServlet {
                     }
 
                     // for(Part part : req.getParts()) {
-                    //     // debug
-                    //     out.println(part.getSubmittedFileName());
+                    // // debug
+                    // out.println(part.getSubmittedFileName());
                     // }
-                    
+
                     // data binding parameters
 
                     Parameter[] methodParams = method.getParameters();
                     Object[] invokationParams = new Object[methodParams.length];
                     int i = 0;
-                    for(Parameter p : methodParams) {
+                    for (Parameter p : methodParams) {
                         String paramName = p.getAnnotation(Params.class).name();
                         Enumeration<String> attrNames = req.getParameterNames();
-                        while(attrNames.hasMoreElements()) {
-                            if(paramName.equals(attrNames.nextElement())) {
+                        while (attrNames.hasMoreElements()) {
+                            if (paramName.equals(attrNames.nextElement())) {
                                 invokationParams[i] = (Utils.strToObject(req.getParameter(paramName), p.getType()));
                                 i++;
                             }
                         }
-                        if(p.getType() == FileUpload.class) {
-                            for(Part part : req.getParts()) {
-                                if(paramName.equals(part.getName())) {
+                        if (p.getType() == FileUpload.class) {
+                            for (Part part : req.getParts()) {
+                                if (paramName.equals(part.getName())) {
                                     FileUpload f = new FileUpload();
                                     f.setName(part.getSubmittedFileName());
                                     f.setBytes(part.getInputStream().readAllBytes());
-    
+
                                     invokationParams[i] = f;
                                     i++;
                                 }
@@ -195,14 +192,14 @@ public class FrontServlet extends HttpServlet {
                     }
 
                     // session binding
-                    
-                    if(method.isAnnotationPresent(Session.class)) {
+
+                    if (method.isAnnotationPresent(Session.class)) {
                         Field sessionField = o.getClass().getDeclaredField("sessions");
                         sessionField.setAccessible(true);
-                        HashMap<String, Object> sessions = (HashMap<String, Object>)sessionField.get(o);
+                        HashMap<String, Object> sessions = (HashMap<String, Object>) sessionField.get(o);
 
                         Enumeration<String> names = req.getSession().getAttributeNames();
-                        while(names.hasMoreElements()) {
+                        while (names.hasMoreElements()) {
                             String sessionName = names.nextElement();
                             Object sessionObject = req.getSession().getAttribute(sessionName);
                             sessions.put(sessionName, sessionObject);
@@ -211,73 +208,77 @@ public class FrontServlet extends HttpServlet {
 
                     // auth
 
-                    ModelView view = null;
+                    if (method.isAnnotationPresent(Api.class) == false) {
+                        ModelView view = null;
 
-                    if(method.isAnnotationPresent(Auth.class)) {
-                        HttpSession servletSession = req.getSession();
-                        String authProfile = method.getAnnotation(Auth.class).profile();
-                        String sessionKey = this.getInitParameter("sessionname");
-                        
-                        String currentUser = (String) servletSession.getAttribute(sessionKey);
-                        if(currentUser == null) {
-                            throw new ServletException("Invalid session");
-                        }
-                        else {
-                            if(currentUser.equals(authProfile)) {
-                                view = (ModelView) method.invoke(o, invokationParams);
-                            }
-                            else if(currentUser.equals(authProfile) == false && authProfile.equals("")) {
-                                view = (ModelView) method.invoke(o, invokationParams);
-                            }
-                            else if(currentUser.equals(authProfile) == false && authProfile.equals("") == false) {
+                        if (method.isAnnotationPresent(Auth.class)) {
+                            HttpSession servletSession = req.getSession();
+                            String authProfile = method.getAnnotation(Auth.class).profile();
+                            String sessionKey = this.getInitParameter("sessionname");
+
+                            String currentUser = (String) servletSession.getAttribute(sessionKey);
+                            if (currentUser == null) {
                                 throw new ServletException("Invalid session");
+                            } else {
+                                if (currentUser.equals(authProfile)) {
+                                    view = (ModelView) method.invoke(o, invokationParams);
+                                } else if (currentUser.equals(authProfile) == false && authProfile.equals("")) {
+                                    view = (ModelView) method.invoke(o, invokationParams);
+                                } else if (currentUser.equals(authProfile) == false
+                                        && authProfile.equals("") == false) {
+                                    throw new ServletException("Invalid session");
+                                }
                             }
                         }
-                    }
 
-                    else if(method.isAnnotationPresent(Auth.class) == false) {
-                        view = (ModelView) method.invoke(o, invokationParams);
-                    }
-
-                    if(view != null) {
-                        HashMap<String, Object> data = view.getData();
-
-                        for (Map.Entry<String,Object> reqData : data.entrySet()) {
-                            req.setAttribute(reqData.getKey(), reqData.getValue());
+                        else if (method.isAnnotationPresent(Auth.class) == false) {
+                            view = (ModelView) method.invoke(o, invokationParams);
                         }
 
-                        HashMap<String, String> sessions = view.getSessions();
-                        for(Map.Entry<String, String> session : sessions.entrySet()) {
-                            req.getSession().setAttribute(session.getKey(), session.getValue());
-                        }
-                    }
+                        if (view != null) {
+                            HashMap<String, Object> data = view.getData();
 
-                    // session binding
-                    
-                    // debug
-                    // out.println(method.isAnnotationPresent(Session.class));
-                    if(method.isAnnotationPresent(Session.class)) {
-                        Field sessionField = o.getClass().getDeclaredField("sessions");
-                        sessionField.setAccessible(true);
-                        HashMap<String, Object> sessions = (HashMap<String, Object>)sessionField.get(o);
-                        
-                        for(Map.Entry<String, Object> s : sessions.entrySet()) {
-                            req.getSession().setAttribute(s.getKey(), s.getValue());
+                            for (Map.Entry<String, Object> reqData : data.entrySet()) {
+                                req.setAttribute(reqData.getKey(), reqData.getValue());
+                            }
+
+                            HashMap<String, String> sessions = view.getSessions();
+                            for (Map.Entry<String, String> session : sessions.entrySet()) {
+                                req.getSession().setAttribute(session.getKey(), session.getValue());
+                            }
                         }
+
+                        // session binding
+
                         // debug
-                        // out.println(req.getSession().getAttribute("nantesession"));
+                        // out.println(method.isAnnotationPresent(Session.class));
+                        if (method.isAnnotationPresent(Session.class)) {
+                            Field sessionField = o.getClass().getDeclaredField("sessions");
+                            sessionField.setAccessible(true);
+                            HashMap<String, Object> sessions = (HashMap<String, Object>) sessionField.get(o);
 
-                    }
+                            for (Map.Entry<String, Object> s : sessions.entrySet()) {
+                                req.getSession().setAttribute(s.getKey(), s.getValue());
+                            }
+                            // debug
+                            // out.println(req.getSession().getAttribute("nantesession"));
 
-                    if(view.isJson()) {
-                        Gson json = new Gson();
-                        String data = json.toJson(view.getData());
-                        res.setContentType("application/json");
-                        out.print(data);
-                    }
-                    else {
-                        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/views/"+view.getView());
-                        dispatcher.forward(req, res);
+                        }
+
+                        if (view.isJson()) {
+                            Gson json = new Gson();
+                            String data = json.toJson(view.getData());
+                            res.setContentType("application/json");
+                            out.print(data);
+                        } else {
+                            RequestDispatcher dispatcher = getServletContext()
+                                    .getRequestDispatcher("/views/" + view.getView());
+                            dispatcher.forward(req, res);
+                        }
+                    } else if(method.isAnnotationPresent(Api.class)) {
+                        Object result = method.invoke(o, invokationParams);
+                        String response = new Gson().toJson(result);
+                        out.print(response);
                     }
 
                 } catch (Exception e) {
@@ -291,6 +292,7 @@ public class FrontServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         processRequest(req, res);
     }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         processRequest(req, resp);
